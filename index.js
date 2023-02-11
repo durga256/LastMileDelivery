1b1fe1191c433a13c99c7c77f38c14a22b17a775
JohnWick08 <mgao041@uottawa.ca>
2023-02-11 16:52:18 -0500

    don't

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function showGrid() {
         let x = document.getElementById('grid');
         x.innerHTML="services";
         const p1 = [1,2,3,4,5,6,7,8,9,10];//this is our arr of arrs of driver nodes
-	    var gridRC = Math.ceil(Math.sqrt(p1.length));
+        var gridRC = Math.ceil(Math.sqrt(p1.length));
         var iframe = '<div class="h_iframe"><iframe class="container" frameborder="0" style="border:0" referrerpolicy="no-referrer-when-downgrade" src="https://www.google.com/maps/embed/v1/directions?key=AIzaSyCYx3Pg-AjHgBYOwJ6LfXpBmuKGWwvH6k8 &origin=ChennaiAirport+India &destination=ChennaiCentral+India &waypoints=Nungambakkam+India|Kodambakkam+India &avoid=tolls|highways" allowfullscreen> </iframe></div>'
         var caption = '<div class="caption">driver';
         var $grid = $('.grid');
@@ -19,43 +19,42 @@ function showGrid() {
                     mapCount += 1;
                 }else{
                     row += '<div class = "square">'+'</div>';
-                }
-            }
+                }}
 
-	        row += '</div>';
+        row += '</div>';
 
-	        $grid.append(row);
-	        console.log(row);
-            console.log($grid);
-        }
+        $grid.append(row);
+        console.log(row);
+        console.log($grid);
+    }
 
 
-        //trying to get the co-ordinates of the location entered using geo
-        //function codeAddress() {
-        //    var address = document.getElementById('address').value;
-        //    geocoder.geocode( { 'address': address}, function(results, status) {
-        //      if (status == 'OK') {
-        //        map.setCenter(results[0].geometry.location);
-        //        var marker = new google.maps.Marker({
-        //            map: map,
-        ///            position: results[0].geometry.location
-        //        });
-        //      } else {
-        //        alert('Geocode was not successful for the following reason: ' + status);
-        //      }
-        //    });
-        //  }
-        
-        //codeAddress()
-        /* New Stuff */
-        var width = 100 / gridRC + '%';
-        var height = 100 / gridRC + 'vh';
-        $('.square').css({'width': width, 'height': height});
+    //trying to get the co-ordinates of the location entered using geo
+    //function codeAddress() {
+    //    var address = document.getElementById('address').value;
+    //    geocoder.geocode( { 'address': address}, function(results, status) {
+    //      if (status == 'OK') {
+    //        map.setCenter(results[0].geometry.location);
+    //        var marker = new google.maps.Marker({
+    //            map: map,
+    ///            position: results[0].geometry.location
+    //        });
+    //      } else {
+    //        alert('Geocode was not successful for the following reason: ' + status);
+    //      }
+    //    });
+    //  }
+
+    //codeAddress()
+    /* New Stuff */
+    var width = 100 / gridRC + '%';
+    var height = 100 / gridRC + 'vh';
+    $('.square').css({'width': width, 'height': height});
+
 }
 
 
 
-openInfo(event,'bookService');
 function openInfo(evt, tabName) {
 
     // Get all elements with class="tabcontent" and hide them
